c0b7e1778d546e74091cad7c2e777c37384b1e81
Marco Ramírez <marco.ramirez@rzerocorp.com>
2019-07-04 18:28:06 -0500

    Initial Commit - Release Candidate

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Phaser from 'phaser';
+import config from './config/config';
+
+import './style.scss';
+
+class Game extends Phaser.Game {
+    constructor() {
+        const docElement = document.documentElement
+        const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
+        const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
+
+        super(width, height, Phaser.CANVAS, 'content', null)
+    }
+}
+
+window.game = new Game();
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                    options: { minimize: true }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
